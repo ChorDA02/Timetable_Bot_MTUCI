92323daa8f638d3ad4665182a2ba366504a81528
SuperDimas2 <79046421+SuperDimas2@users.noreply.github.com>
2022-07-07 15:59:29 +0300

    Added mysql_connect.py

---
+++ mysql_connect.py
@@ -0,0 +1,5 @@
+# Insert your MySQL login details here
+host = "YOUR HOST HERE" # Example: 127.0.0.1
+user = "YOUR USER HERE" # Example: user
+password = "YOUR PASSWORD HERE" # Example: V3ry5tr0ngPa55w0rd
+database = "YOUR DATABASE HERE" # 'para' by default, replace if necessary
